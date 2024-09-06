119da604a8b4f4e0f9c186267b8d52ef5ed47a78
HAMZA-NASIR-PU <hamzanasir1111.hn@gmail.com>
2024-09-06 10:49:26 +0500

    Job Portal - redirection in server components

--- src/app/account/page.js
+++ src/app/account/page.js
@@ -1,3 +1,13 @@
-export default function Account(){
-    return <h1>Account Page</h1>
+import { redirect } from "next/navigation";
+
+export default function Account() {
+    //assume that profile info is null
+
+    const userProfileInfo = null;
+
+    if (!userProfileInfo) { 
+        redirect('profile');   
+    }
+
+    return <h1>Accounts Page</h1>
 }
--- src/app/page.js
+++ src/app/page.js
@@ -19,11 +19,11 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       HELLO WORLD
 
-      {/* <Link href={`products`}>Navigate to products page</Link>
-      <Link href={`account`}>Navigate to Accounts page</Link> */}
+      <Link href={`products`}>Navigate to products page</Link>
+      <Link href={`account`}>Navigate to Accounts page</Link>
 
 
-      <button onClick={handleNavigate}>Navigate to Products Page using useRouter hook</button>
+      {/* <button onClick={handleNavigate}>Navigate to Products Page using useRouter hook</button> */}
 
 
     </div>
